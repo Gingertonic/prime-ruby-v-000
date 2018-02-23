b2ee7be2876ac7fa42dac5031955cf96c9232c0f
Beth Schofield <bethmschofield@gmail.com>
2018-02-23 02:53:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
 # Add  code here!
 def prime?(num)
-  check = (1...num).to_a
-  check.each do |x|
-    if x * x = num
-      return true
-    else
-      return false
+  if num < 0
+    return false
+  else
+    check = (1...num).to_a
+    check.each do |x|
+      if x * x = num
+        return true
+      else
+        return false
+      end
     end
   end
 end
