859deec7203bdea1bde15afb95f9ba0b1f75a15f
Beth Schofield <bethmschofield@gmail.com>
2018-02-23 03:20:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,40 @@
-# Add  code here!
-def square?(num)
+# # Add  code here!
+# def square?(num)
+#   if num <= 1
+#     return false
+#   else
+#     check = (1...num).to_a
+#     yesno = check.find {|x| (x * x == num)}
+#     !yesno
+#   end
+# end
+
+def prime?(num)
   if num <= 1
     return false
   else
     check = (1...num).to_a
-    yesno = check.find {|x| (x * x == num)}
-    !yesno
+    y += 0
+    check.each do |x|
+      while y < check.length do
+        if check[y] * x == num
+          puts "Try again"
+        else
+          return true
+        end
+        y += 1
+      end
+    end
   end
 end
+
+# def find_the_cheese(recipe) 
+#   cheese_types = ["cheddar", "gouda", "camembert"] 
+#   recipe.each do |ingr|
+#     if cheese_types.include?(ingr)
+#      return ingr
+#     else
+#      puts "No cigar"
+#     end
+#   end
+# end
